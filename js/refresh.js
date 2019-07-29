987897e62af7a2e92a95967593ddae8fe80760ad
KI <ghk829@hanmail.net>
2019-07-29 20:36:10 +0900

    refresh event with toastr

--- js/refresh.js
+++ js/refresh.js
@@ -1,8 +1,21 @@
+function refreshMessage(){
+    toastr.info("refresh");
+    return new Promise(function(resolve,reject){
+        resolve();
+    })
+}
+
 function refreshStorage(){
+    
     chrome.storage.sync.clear();
     chrome.storage.local.clear();
-    alert("Clean All Clipboard!!")
-    window.location.reload();
+
+    refreshMessage().then(function(){
+        setTimeout(function(){
+            window.location.reload();
+        },1000)
+        
+    })
 }
 
 var refresh = document.getElementById("refresh");
